36dba2f5d2a55dfea90ca9f0fb1efb2334ea33ff
Nikhil <nikhil@wolkensoftware.com>
2024-06-22 12:07:23 +0530

    initial code push of cinimate

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,19 @@
-export const MovieList = () => {
+
+import { Card } from "../components"
+import { useFetch } from "../hooks/useFetch";
+export const MovieList = ({apiPath}) => {
+  console.log("MovieList apiPath",apiPath);
+  const {data : movies} = useFetch(apiPath);
+  
   return (
-    <main className=''>MovieList</main>
+    <main >
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies && movies.map((movie)=>(
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
   )
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,24 @@
+import { useSearchParams } from "react-router-dom";
+import { Card } from "../components"
+import { useFetch } from "../hooks/useFetch";
 
-export const Search = () => {
+export const Search = ({apiPath}) => {
+  const [searchParam] = useSearchParams();
+  const queryTerm = searchParam.get("q");
+
+  const {data : movies} = useFetch(apiPath,queryTerm);
   return (
-    <main>Search</main>
+    <main >
+      <section className="py-7">
+        <p className="text-3xl text-gray-700 dark:text-white"> {movies.length === 0 ? `No result found for ${queryTerm}` : `Result for ${queryTerm}`} </p>
+      </section>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          {movies && movies.map((movie)=>(
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
   )
 }
